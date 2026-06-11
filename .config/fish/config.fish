4fe89b1a472ede64566c396013008b222d4514dd
Lonelyyyice <pychen117@163.com>
2026-06-11 20:29:54 +0800

    fix:fish_config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,25 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    # fastfetch --logo ~/Pictures/your_icon.png --logo-type kitty --logo-width 30
+	alias c="clear"
+	alias f="set img (ls ~/Pictures/fastfetch-random/*.{jpg,png,jpeg,webp} 2>/dev/null | shuf | head -n1); fastfetch --logo \$img --logo-type kitty --logo-width 30"
+	alias cf="clear && f"
+	alias lt="eza --tree --level=2 --icons"
+	alias ls="eza --icons"
 end
+
+# Created by `pipx` on 2025-12-27 13:31:29
+set PATH $PATH /home/cyp/.local/bin
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/cyp/miniconda3/bin/conda
+    eval /home/cyp/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/cyp/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/cyp/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/cyp/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
+starship init fish | source
